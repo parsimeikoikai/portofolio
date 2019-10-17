67177d4183deef060dfc7e98f08f9fdcc8362baa
parsimeikoikai <55235172+parsimeikoikai@users.noreply.github.com>
2019-10-17 08:41:28 +0300

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,237 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>ROBERT KOIKAI</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Experience</a></li>
+							<li><a href="#two">Projects</a></li>
+							<li><a href="#three">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Robert Parsimei Koikai</h1>
+							<p><b>Full Stack Web Developer.</b><br>
+								Experienced software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success.
+							</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Read  more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>The Aghakhan University(TREE Program)</h2>
+									<h5>Web Applications Developer: May 2019 - Present</h5>
+									<p>
+									<ul>
+									<li>Use open-source technologies to enhance TrainSMART and Moodle, specifically development in PHP , MySQL and Angular JS.</li>
+									<li>Perform direct development and management of reliable and secure environments for production deployments, including the use of virtual machines, openVPN, SSH, etc.</li>
+									<li>Assist with developing technical documentation, as needed.</li>
+									<li>Develop CME functionality</li>
+									<li>Develop Appropriate API’s to enable integration of various systems used withing the organisation.</li>
+
+								</ul>
+									</p>
+
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Futuristic Limited</h2>
+									<h5>Web Applications Developer: April 2018 - April 2019</h5>
+									<p>
+									<ul>
+										<li>Software Developer-A full time web developer. </li>
+										<li>Bulk SMS and API integration .</li>
+										<li>System Design and Documentation.</li>
+										<li>Software testing and presentation </li>
+										<li>Maintenance,training and support of existing systems.</li>
+
+									</ul>
+									</p>
+
+
+
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>National Aids and Sti Control Program (NASCOP).</h2>
+									<h5>IT Support Technician Intern: October 2017 - February 2018</h5>
+									<p>
+									<ul>
+										<li>Participate in field mentorship and capacity building exercises.</li>
+										<li>Conduct Discrepancy checks between data entry records,platform error checks and other data quality checks on the entered data.</li>
+										<li>Participate in development of specifications, testing and installation of various software’s.</li>
+										<li>Conduct Discrepancy checks between data entry records,platform error checks and other data quality checks on the entered data. </li>
+
+
+									</ul>
+									</p>
+
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>PORTOFOLIO</h2>
+							<p>Below you can see some projects I've been working on lately. I divide each project into four follow-up areas: comprehensive research, wireframing, design & development. This process allows me to create great product with client goals in mind.</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>PrEP Lab Seroconvetors System</h3>
+									<p>A web-based application that collects and analyses data.This data is collected in all stations over the country</p>
+									<p style="color: blue"><a target="_blank" href="https://seroconverters.nascop.org">Click Here</a></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>TrackSoft system</h3>
+									<p>CargoWise One is an environment of integrated enterprise resource planning applications. It integrates forwarding, costing, and invoicing requirements into one multi-layered form. The environment allows you to create and send many types of declarations. It automatically links these declarations to shipment details within the system. .</p>
+									<p style="color: blue"><a target="_blank" href="https://www.futuristicdevworks.com/ssudandev/">Click Here</a></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>Teacher Perfomance Appraisal and Development(TPAD)</h3>
+									<p>TPAD which is an open appraisal system allows teachers in primary and secondary schools to participate in evaluating their own performance and initiate their professional development.</p>
+									<p style="color: blue"><a target="_blank" href="https://www.tpad2.tsc.go.ke">Click Here</a></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>Training System Monitoring and Reporting Tool(TrainSMART)</h3>
+									<p>This is an open-source, web-based training data collection system. It allows users to accurately track data about training programs, trainers, and trainees, to better evaluate programs and report activities to stakeholders.</p>
+									<p style="color: blue"><a target="_blank" href="http://trainsmart.nascop.org/trainsmartprod">Click Here</a></p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>CEMES Insurance Agency</h3>
+									<p>A prospective customer can perform filters and enquiries for insurance products,this data is then sent to 
+ agent mobile application that shall be used by the insurance sales agents to sell
+insurance policies.</p>
+									<p style="color: blue"><a target="_blank" href="https://www.cemesinsurance.co.ke/">Click Here</a></p>
+								</section>
+								
+							</div>
+
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Get in touch</h2>
+							<p> I can be contacted through.</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Name</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Email</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Message</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<span>P.O BOX 252 <br />
+											KAJIADO<br />
+											KENYA</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="#">krober690@gmail</a>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<span>0704065183</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://twitter.com/_parsimei" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="https://www.facebook.com/robart.koikai" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.instagram.com/roba_kay/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://www.linkedin.com/in/parsimei-koikai-93467980/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Robert Koikai. All rights reserved.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
